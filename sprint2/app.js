7f290c1ef237511396a0341beb4a35cc7ab2d332
Aleex1237 <alexis10893@outlook.com>
2021-06-19 16:04:43 -0300

    commit inicial rama

---
+++ sprint2/app.js
@@ -0,0 +1,33 @@
+const express=require("express");
+const app=express();
+const path=require("path");
+const port=3000;
+app.use(express.static("public"));
+
+app.get("/", (req, res)=>{
+    res.sendFile(path.join(__dirname, "views","index.html"))
+})
+
+app.get("/productDetail", (req, res)=>{
+    res.sendFile(path.join(__dirname, "views","productDetail.html"))
+})
+
+app.get("/productCart", (req, res)=>{
+    res.sendFile(path.join(__dirname, "views","productCart.html"))
+})
+
+app.get("/register", (req, res)=>{
+    res.sendFile(path.join(__dirname, "views","register.html"))
+})
+
+app.get("/login", (req, res)=>{
+    res.sendFile(path.join(__dirname, "views","login.html")) 
+})
+
+app.get("/productLoad", (req, res)=>{
+    res.sendFile(path.join(__dirname, "views","productLoad.html")) 
+})
+
+app.listen(port,()=>{
+    console.log(`El proyecto Home Experience se está ejecutando en localhost:${port}`)
+})
