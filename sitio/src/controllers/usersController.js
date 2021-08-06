19bdb3b9a313d9382e838e8390e166844bab87ae
Aleex1237 <alexis10893@outlook.com>
2021-08-06 19:34:32 -0300

    agregando admin view

--- sitio/src/controllers/usersController.js
+++ sitio/src/controllers/usersController.js
@@ -1,14 +1,24 @@
-module.exports= {
+const { leer } = require("../data/products_db");
+let products = leer();
 
-    login: (req,res) => {
-        return res.render('login',{
-            title: "Iniciar sesión",
-        })
-    },
+module.exports = {
+  login: (req, res) => {
+    return res.render("login", {
+      title: "Iniciar sesión",
+    });
+  },
 
-    register: (req,res) => {
-        return res.render('register',{
-            title: "Registrate",
-        })
-    }
-}
+  register: (req, res) => {
+    return res.render("register", {
+      title: "Registrate",
+    });
+  },
+  admin: (req, res) => {
+    let id = req.params.id;
+
+    return res.render("adminView", {
+      title: "Administrador",
+      products,
+    });
+  },
+};
