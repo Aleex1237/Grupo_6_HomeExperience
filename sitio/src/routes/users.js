f05c61aa81214eec5a128b44221cbdf912c62cb8
marciaDelCastillo <marcia990_14@hotmail.com>
2021-08-17 10:58:30 -0300

    register validaciones y guardado en json

--- sitio/src/routes/users.js
+++ sitio/src/routes/users.js
@@ -2,15 +2,17 @@ var express = require("express");
 var router = express.Router();
 
 //requriendo controladores
-const { login, register, admin, logUser } = require("../controllers/usersController");
+const { login, register, admin, logUser, addUser } = require("../controllers/usersController");
 
 //requiriendo middlewares
 const loginValidator = require("../validations/loginValidator");
+const registerValidator = require("../validations/registerValidator");
 
 /* GET users listing. */
 router.get("/iniciar-sesion" , login);
 router.post("/iniciar-sesion", loginValidator, logUser)
 
 router.get("/registro", register);
+router.post("/registro", registerValidator, addUser);
 
 module.exports = router;
