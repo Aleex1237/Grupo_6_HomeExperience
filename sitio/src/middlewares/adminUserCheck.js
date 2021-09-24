4d053a8c960bdf2715d58a633f15393a33dacca7
Aleex1237 <alexis10893@outlook.com>
2021-09-24 11:19:47 -0300

    agregando middelwares

--- sitio/src/middlewares/adminUserCheck.js
+++ sitio/src/middlewares/adminUserCheck.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-  if (res.locals.user && res.locals.user.admin === 2) { 
+  if (res.locals.user && (res.locals.user.idRol == 1 || res.locals.user.idRol == 2)) { 
     //si en locals existe userLogin y en userLogin.admin es true entonces prosigue al siguiente middleware o el controlador
     next();
   } else {
