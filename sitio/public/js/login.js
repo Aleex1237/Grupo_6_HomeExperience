cfdcde66d34214e325f999c575ba58d381b35206
Aleex1237 <alexis10893@outlook.com>
2021-10-12 18:26:07 -0300

    arreglando error de llaves

--- sitio/public/js/login.js
+++ sitio/public/js/login.js
@@ -1,55 +1,47 @@
+window.addEventListener("load", () => {
+  console.log("CONECTADO SATISFACTORIAMENTE CON LOGIN");
 
-<<<<<<< HEAD
-/* window.addEventListener('load', () => {
-    console.log('CONECTADO SATISFACTORIAMENTE CON LOGIN');
-=======
->>>>>>> 0bf42785483dbb84ebcacdc3c8bbee6049f7b78b
+  const $ = (id) => document.getElementById(id);
 
-const $= id => document.getElementById(id)
+  $("floatingInput").addEventListener("blur", () => {
+    if (!regExEmail.test($("floatingInput").value)) {
+      $("floatingInput").classList.add("is-invalid");
+      $("error-mail").innerHTML = "Ingrese un correo electrónico válido";
+    } else {
+      $("floatingInput").classList.remove("is-invalid");
+      $("floatingInput").classList.add("is-valid");
+      $("error-mail").innerHTML = null;
+    }
+  });
 
+  let password = document.getElementById("floatingPassword");
 
-    $('floatingInput').addEventListener('blur', () => {
-        if(!regExEmail.test($('floatingInput').value)){
-            $('floatingInput').classList.add('is-invalid')
-            $('error-mail').innerHTML = "Ingrese un correo electrónico válido";
-        } else{
-            $('floatingInput').classList.remove('is-invalid')
-            $('floatingInput').classList.add('is-valid')
-            $('error-mail').innerHTML = null
+  password.addEventListener("blur", () => {
+    if (!password.value) {
+      password.classList.add("is-invalid");
+      $("error-password").innerHTML = "Ingrese su contraseña";
+    } else {
+      password.classList.remove("is-invalid");
+      password.classList.add("is-valid");
+      $("error-password").innerHTML = null;
+    }
+  });
 
-        }
-    })
-    
-let password= document.getElementById('floatingPassword')
-
-
-    password.addEventListener('blur', () => {
-        if(!password.value){
-            password.classList.add('is-invalid')
-            $('error-password').innerHTML = "Ingrese su contraseña";
-        }else{
-            password.classList.remove('is-invalid')
-            password.classList.add('is-valid')
-            $('error-password').innerHTML = null
-
-        }
-    }) 
-
-
-$('form-login').addEventListener('submit', e => {
+  $("form-login").addEventListener("submit", (e) => {
     e.preventDefault();
-  
-    let elementosForm= $('form-login').elements 
-    let error= false;
-  
-    for (let i = 0; i < elementosForm.length -1; i++) {
-      
+
+    let elementosForm = $("form-login").elements;
+    let error = false;
+
+    for (let i = 0; i < elementosForm.length - 1; i++) {
       if (!elementosForm[i].value) {
-        elementosForm[i].classList.add('is-invalid')
-        $('error-vacio').innerHTML= '<br>Quedan campos por completar';
-        error= true;
+        elementosForm[i].classList.add("is-invalid");
+        $("error-vacio").innerHTML = "<br>Quedan campos por completar";
+        error = true;
       }
       if (!error) {
-        $('form-login').submit()
+        $("form-login").submit();
       }
-    }})
+    }
+  });
+});
